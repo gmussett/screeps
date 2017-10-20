cc41e91b4c2fb9d7038f79442a2ee29591139b9b
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-20 15:26:43 -0400

    tutorial 1: add roleHarvester role

--- prod/main.js
+++ prod/main.js
@@ -1,5 +1,23 @@
 // spawn syntax: Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
 
+
+var roleHarvester = {
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+	}
+};
+
 module.exports.loop = function () {
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
