ced66bb6da55ca0182f36198c256a1c53cce64c5
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-20 15:33:46 -0400

    update spawn comments

--- prod/main.js
+++ prod/main.js
@@ -1,4 +1,10 @@
-// spawn syntax: Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+// spawn syntax:
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester2' );
+// Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1' );
+
+// Game.creeps['Harvester1'].memory.role = 'harvester';
+// Game.creeps['Upgrader1'].memory.role = 'upgrader';
 
 var roleHarvester = require('role.harvester');
 
