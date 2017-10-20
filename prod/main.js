9401c716eb51fb74022f92b86a55ea0c992f306d
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-20 17:10:50 -0400

    update for autospawn

--- prod/main.js
+++ prod/main.js
@@ -13,6 +13,9 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
@@ -20,9 +23,6 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-           roleBuilder.run(creep);
         }
     }
 }
