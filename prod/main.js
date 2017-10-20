a6a5061a23559470981acb4d5a2fcca3ac9e8a92
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-20 17:21:17 -0400

    add memory garbage collection

--- prod/main.js
+++ prod/main.js
@@ -15,6 +15,12 @@ var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
