f067d0878c670dbed348dedf202cb1d1244d66ed
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-20 15:34:33 -0400

    link new upgrader role

--- prod/main.js
+++ prod/main.js
@@ -7,11 +7,17 @@
 // Game.creeps['Upgrader1'].memory.role = 'upgrader';
 
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
     }
 }
