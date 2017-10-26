baebb3aa862a1948492e305c5339b009a720d8af
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-26 13:00:25 -0400

    make idle harvesters wait at spawner

--- prod/role.harvester.js
+++ prod/role.harvester.js
@@ -5,6 +5,7 @@ var roleHarvester = {
 	    if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.say('harvest');
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
@@ -18,8 +19,13 @@ var roleHarvester = {
             });
             if(targets.length > 0) {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.say('store');
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            } else {
+              spawns = creep.room.find(FIND_MY_SPAWNS);
+              creep.say('idle');
+              creep.moveTo(spawns[0])
             }
         }
 	}
