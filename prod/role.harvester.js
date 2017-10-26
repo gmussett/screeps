e38b53b5ee536c780ed7e888e9ed9750157a32c9
Geoff Mussett <geoff.mussett@nasdaq.com>
2017-10-26 13:12:41 -0400

    clean up harvester code

--- prod/role.harvester.js
+++ prod/role.harvester.js
@@ -8,32 +8,32 @@ var roleHarvester = {
                 creep.say('harvest');
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.say('store');
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            } else {
-              flags = creep.room.find(FIND_FLAGS);
-              if (flags.length > 0) {
-                creep.moveTo(flags[0]);
-              } else {}
-                spawns = creep.room.find(FIND_MY_SPAWNS);
-                creep.say('idle');
-                creep.moveTo(spawns[0])
-              };
+        } else {
+          var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+              return (structure.structureType == STRUCTURE_EXTENSION ||
+                structure.structureType == STRUCTURE_SPAWN ||
+                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+              }
             }
+          );
+          if(targets.length > 0) {
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.say('store');
+              creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+          } else {
+            flags = creep.room.find(FIND_FLAGS);
+            if (flags.length > 0) {
+              creep.moveTo(flags[0]);
+            } else {
+              spawns = creep.room.find(FIND_MY_SPAWNS);
+              creep.say('idle');
+              creep.moveTo(spawns[0])
+            }
+          }
         }
-	}
+    }
 };
 
 module.exports = roleHarvester;
